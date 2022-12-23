4f11c4d66f59ae59a96766b1f84b561089d7a97e
Matan Brightbert <matana0000@gmail.com>
2022-12-23 19:01:58 +0200

    Added files

---
+++ SilentRunner/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+
+
+namespace SilentRunner
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Process process = new Process();
+            if (args.Length > 0)
+            {
+                process.StartInfo.FileName = args[0];
+                if (args.Length > 1)
+                    process.StartInfo.Arguments = args[1];
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.CreateNoWindow = true;
+                process.Start();
+            }
+        }
+    }
+}
